87d13b3e3e7e8207d80afdf6a3f9dcb6385f3f7f
Dilshad <muhammaddilshad2167@gmail.com>
2026-01-01 14:24:45 +0500

    create slice and import in store

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import authReducer from "./Auth/Slice"
 export const store =configureStore({
-    reducer: {},
+    reducer: {
+        auth: authReducer ,
+    },
 })
